2f2111b7d4f3a6a2a3f37ea3fee22475b3f6ac18
AppDeveloperVM <vicmr1994@gmail.com>
2022-12-22 17:18:09 +0100

    Add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'kubide-heroes',
+    appId: '1:833258792063:web:2c8b37d6219722cba20b4a',
+    storageBucket: 'kubide-heroes.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyC4XTjWsGD4FpmiAnzXCeatVmQxazNSg5Y',
+    authDomain: 'kubide-heroes.firebaseapp.com',
+    messagingSenderId: '833258792063',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'kubide-heroes',
+    appId: '1:833258792063:web:2c8b37d6219722cba20b4a',
+    storageBucket: 'kubide-heroes.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyC4XTjWsGD4FpmiAnzXCeatVmQxazNSg5Y',
+    authDomain: 'kubide-heroes.firebaseapp.com',
+    messagingSenderId: '833258792063',
+  },
   production: false
 };
 

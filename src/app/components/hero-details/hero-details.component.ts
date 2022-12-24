b9fd551e8465cdbc4e6865ea6ab1217a3b1c4904
AppDeveloperVM <vicmr1994@gmail.com>
2022-12-24 21:04:47 +0100

    Updated hero details page - modal

--- src/app/components/hero-details/hero-details.component.ts
+++ src/app/components/hero-details/hero-details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ImageVariant } from 'src/app/models/Image.model';
+import { MarvelAPIProviderService } from 'src/app/providers/marvel-apiprovider.service';
 
 @Component({
   selector: 'app-hero-details',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./hero-details.component.scss']
 })
 export class HeroDetailsComponent {
+  @Input() added: Boolean = true;
+  @Input() characterData : any;
+  thumbnail : string;
+
+  constructor(public modalCtrl : ModalController, public marvelApi : MarvelAPIProviderService){ }
+
+  ngOnInit(){
+    this.getImage(this.characterData);
+  }
+
+  getImage(character: any) {
+    const response = this.marvelApi.getImage(character.thumbnail, ImageVariant.portrait_incredible);
+    this.thumbnail = response;
+  }
+
+  close(){
+    this.modalCtrl.dismiss();
+  }
 
 }

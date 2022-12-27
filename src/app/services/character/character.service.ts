83f593936e8b3bd35604e25ae6e6434c5ac3480c
AppDeveloperVM <vicmr1994@gmail.com>
2022-12-28 00:09:24 +0100

    Update character.service.ts

--- src/app/services/character/character.service.ts
+++ src/app/services/character/character.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+import { TeamService } from '../team/team.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharacterService {
+  //private added = new BehaviorSubject<boolean>(false);
+  //added$ = this.added.asObservable();
+  private character = new BehaviorSubject<any>([]);
+  character$ = this.character.asObservable();
 
-  constructor() { }
+  private isCharacterAdded = new BehaviorSubject<boolean>(false);
+  isCharacterAdded$ = this.isCharacterAdded.asObservable();
+
+  constructor(public teamService: TeamService) {
+   // this.teamService.checkHeroInTeam()
+
+    this.character.subscribe((res)=> {
+      //console.log(res);
+    })
+  }
+
+  setCharacterSelected(character: any){
+    this.character.next(character);
+  }
+
+  setCharacterAdded(isAdded){
+    this.isCharacterAdded.next(isAdded);
+  }
+
 }
